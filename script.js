8d5e8f6d8366789e7ad5f68128b6cbd26745b84f
Andi <andi_cakolli@outlook.com>
2023-11-30 17:51:06 -0800

    Shembulli mouseup - mousedown

---
+++ script.js
@@ -0,0 +1,11 @@
+function mouseDown() {
+    var b = document.getElementById("myP");
+    b.style.color = "red";
+    b.innerHTML = "Ky tekst ka ngjyren e kuqe";
+}
+
+function mouseUp() {
+    var a = document.getElementById("myP");
+    a.style.color = "blue";
+    a.innerHTML = "Ky tekst ka ngjyren e kaltert";
+}
